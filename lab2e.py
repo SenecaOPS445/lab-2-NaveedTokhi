fd9a98c97ae14035bc9caa2cf848fc40fac9615d
Naveed Tokhi <dmuhammad4@myseneca.ca>
2025-01-24 03:19:31 -0500

    Lab 2 completed

---
+++ lab2e.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+
+#Author: Naveed Tokhi]
+#Author: dmuhammad4
+#Date Created: 2025/01/24
+
+timer = 10
+
+while timer > 0 < 10:
+    print(timer)
+    timer -= 1
+print('blast off!')
